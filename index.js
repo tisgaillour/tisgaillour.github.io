0c6eb3a59c7d14387df123e669100fce70c77b39
gaillour <juangaillour.8@gmail.com>
2020-11-15 15:10:01 -0300

    animaciones cambiadas y recarga con boton

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 //Todas las animaciones para que los elementos aparezcan con delay
-ScrollReveal().reveal("#nombre", { delay: 500 })
+ScrollReveal().reveal("#nombre", { delay: 800 })
 ScrollReveal().reveal("#juani", { delay: 800 })
 ScrollReveal().reveal(".cartel", { delay: 500 })
 ScrollReveal().reveal(".logico-matematica",{ delay: 500 })
 ScrollReveal().reveal(".intrapersonal",{ delay: 1000 })
 ScrollReveal().reveal(".creativa", { delay: 1500 })
-ScrollReveal().reveal("#jardin", {delay:500})
-ScrollReveal().reveal("#sala-5", {delay:500})
-ScrollReveal().reveal("#primer-grado", {delay:500})
-ScrollReveal().reveal("#grado-5", {delay:500})
-ScrollReveal().reveal("#grado-6", {delay:500})
-ScrollReveal().reveal("#comienzos", {delay:500})
-ScrollReveal().reveal("#interes", {delay:500})
-ScrollReveal().reveal("#actualidad", { delay: 500 })
+// ScrollReveal().reveal("#jardin", {delay:500})
+// ScrollReveal().reveal("#sala-5", {delay:500})
+// ScrollReveal().reveal("#primer-grado", {delay:500})
+// ScrollReveal().reveal("#grado-5", {delay:500})
+// ScrollReveal().reveal("#grado-6", {delay:500})
+// ScrollReveal().reveal("#comienzos", {delay:500})
+// ScrollReveal().reveal("#interes", {delay:500})
+// ScrollReveal().reveal("#actualidad", { delay: 500 })
 
 // agarrar todos los valores
 var valores = document.getElementsByClassName('valores');
@@ -44,16 +44,15 @@ function getRandomNumber(min, max) {
     
 };
 
-//for loop que recorre a todos los elementos de la lista de valores
-for (i = 0; i < valores.length; i++){
-    //cada vez que pasa le suma 50 a la variable i
-    i += 50
-    //el valor de i se le suma al delay asi va creciendo para que el siguiente valor tarde mas en mostrarse
-    ScrollReveal().reveal(".valores", {delay:500+ i})
-}
+// //for loop que recorre a todos los elementos de la lista de valores
+// for (i = 0; i < valores.length; i++){
+//     //cada vez que pasa le suma 50 a la variable i
+//     i += 50
+//     //el valor de i se le suma al delay asi va creciendo para que el siguiente valor tarde mas en mostrarse
+//     ScrollReveal().reveal(".valores", {delay:500+ i})
+// }
 
 window.onload = function() { 
-		
 		animateprogress("#realista",13);
 		animateprogress("#investigador",20);
 		animateprogress("#artistico",10);
